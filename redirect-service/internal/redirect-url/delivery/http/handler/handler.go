7c9bb4095d7de194a1bb9e0c69c6d0c4a71c1c80
TwIllght-R <nathawutrock@gmail.com>
2025-04-22 21:55:30 +0900

    init redirect-service

---
+++ redirect-service/internal/redirect-url/delivery/http/handler/handler.go
@@ -0,0 +1,53 @@
+package handler
+
+import (
+	"net/http"
+	"redirect-service/domain/entities"
+	"redirect-service/domain/interfaces"
+
+	"github.com/gin-gonic/gin"
+)
+
+type redirectURLHandler struct {
+	redirectURLUsecase interfaces.RedirectURLUsecase
+}
+
+func NewRedirectURLHandler(g *gin.Engine, redirectURLUsecase interfaces.RedirectURLUsecase) {
+	handler := &redirectURLHandler{
+		redirectURLUsecase: redirectURLUsecase,
+	}
+
+	v1 := g.Group("/v1")
+
+	v1.GET("/:short-code", handler.RedirectURL)
+
+}
+
+func mapStatusCode(err error) int {
+	switch err {
+	case entities.ErrURLAlreadyExists:
+		return http.StatusConflict
+	case entities.ErrURLNotFound:
+		return http.StatusNotFound
+	case entities.ErrInternalServer:
+		return http.StatusInternalServerError
+	default:
+		return http.StatusBadRequest
+	}
+}
+
+func (h *redirectURLHandler) RedirectURL(c *gin.Context) {
+	shortCode := c.Param("short-code")
+
+	originalURL, err := h.redirectURLUsecase.Redirect(c.Request.Context(), entities.RedirectURL{
+		ShortCode: shortCode,
+		UserAgent: c.Request.UserAgent(),
+		IP:        c.ClientIP(),
+	})
+	if err != nil {
+		c.JSON(mapStatusCode(err), gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Redirect(302, *originalURL)
+}
