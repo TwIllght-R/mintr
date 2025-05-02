505763d5f4d3b4525a47304f6ed168d56715e095
TwIllght-R <nathawutrock@gmail.com>
2025-05-02 16:17:19 +0900

    update bson

--- redirect-service/domain/entities/redirect_url.go
+++ redirect-service/domain/entities/redirect_url.go
@@ -3,15 +3,15 @@ package entities
 import "time"
 
 type RedirectURL struct {
-	UUID        string
-	ShortCode   string
-	OriginalURL string
-	ExpiresAt   time.Time
-	UTMSource   string
-	UTMMedium   string
-	UTMCampaign string
-	UTMTerm     string
-	UTMContent  string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	UUID        string    `bson:"uuid"`
+	ShortCode   string    `bson:"short_code"`
+	OriginalURL string    `bson:"original_url"`
+	ExpiresAt   time.Time `bson:"expires_at"`
+	UTMSource   string    `bson:"utm_source"`
+	UTMMedium   string    `bson:"utm_medium"`
+	UTMCampaign string    `bson:"utm_campaign"`
+	UTMTerm     string    `bson:"utm_term"`
+	UTMContent  string    `bson:"utm_content"`
+	CreatedAt   time.Time `bson:"created_at"`
+	UpdatedAt   time.Time `bson:"updated_at"`
 }
--- redirect-service/internal/redirect-url/delivery/mq/handler/url_created_event.go
+++ redirect-service/internal/redirect-url/delivery/mq/handler/url_created_event.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"redirect-service/domain/entities"
 	"redirect-service/domain/events"
@@ -25,6 +26,7 @@ func (h *urlCreatedEventHandler) HandleEvent(msg []byte) {
 		log.Println("Error unmarshalling event:", err)
 		return
 	}
+	fmt.Println(evt.UUID)
 	_, err := h.redirectUseCase.CreateRedirectURL(context.Background(), entities.RedirectURL{
 		UUID:        evt.UUID,
 		ShortCode:   evt.ShortCode,
